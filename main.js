5c3e4a12f7ef9727b9ede716e5b550684b244c79
vikash pathak <chotapathak12@gmail.com>
2021-09-05 16:02:53 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+const addButton = document.querySelector('.addButton');
+var inputValue = document.querySelector('.input');
+const container = document.querySelector('.container');
---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1> ABout </h1>
+this is a simple todo list

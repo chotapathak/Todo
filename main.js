f7cc1bec1e9d1ca8ba30d7d5471dcf1c8a79a4b6
Vikash Pathak <chotapathak12@gmail.com>
2022-07-31 00:33:17 +0530

    added transition while deleting task

--- main.js
+++ main.js
@@ -1,46 +1,47 @@
-// Selector
-
-const todoInput = document.querySelector('.todo-input');
+const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoList = document.querySelector('.todo-list')
+todoButton.addEventListener('click', addTask)
 
-
-todoButton.addEventListener('click', addTodo);
-
-function addTodo(e){
+function addTask(e){
     e.preventDefault();
+
     const newDiv = document.createElement('div');
     newDiv.classList.add('todo');
     
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newDiv.append(newTodo);
-    todoList.append(newDiv);
     todoInput.value = '';
 
     const completeButton = document.createElement('button');
-    completeButton.innerHTML = `<i class="fas fa-check"/>`;
-    completeButton.classList.add('complete-btn');
+    completeButton.innerHTML = `<i class='fas fa-check'/>`;
+    completeButton.classList.add('complete-btn')
     newDiv.append(completeButton);
-    // add eventlistener 
-    completeButton.addEventListener('click', checkComplete);
+    completeButton.addEventListener('click', completeTask)
+
 
     const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = `<i class="fas fa-trash"/>`;
-    deleteButton.classList.add('trash-btn');
+    deleteButton.innerHTML = `<i class='fas fa-trash'/>`;
+    deleteButton.classList.add('trash-btn')
     newDiv.append(deleteButton);
-    deleteButton.addEventListener('click', deleteTasks);
+    deleteButton.addEventListener('click', deleteTask)
+
+    todoList.append(newDiv);
+
 }
 
-function checkComplete(e){
+function completeTask(e){
     const item = e.target;
+    item.classList.toggle('completed')
 
-    item.parentElement.classList.toggle('completed');
-    console.log(item.parentElement);
 }
-
-function deleteTasks(e){
+function deleteTask(e){
     const item = e.target;
-    item.parentElement.remove();
-    // console.log(item.parentElement);
+    const todo = item.parentElement;
+    todo.classList.add('fall'); 
+    todo.addEventListener('transitionend', function(){
+        item.parentElement.remove()
+    })
+
 }
